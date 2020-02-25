58ed661d6c4b077f38f0e482eea2faf8887d3682
wuda5 <2252316187@qq.com>
2020-02-25 15:09:04 +0800

    对上次提交的说明（上次的描述有误--> 应该是：FactoryBean 的各种情况的学习）

--- lb_source_code/spring-framework-5.0.x/ circular-reference-wu/src/main/java/com/luban/app/Appconfig.java
+++ lb_source_code/spring-framework-5.0.x/ circular-reference-wu/src/main/java/com/luban/app/Appconfig.java
@@ -1,11 +1,15 @@
 package com.luban.app;
 
+import com.luban.auto.autoModeTypeUpdate.MyImportBeanDefinitionRegistrar;
+import com.luban.imports.MyImportSelectorTest;
+import org.springframework.context.annotation.*;
+
 //import com.luban.imports.MyImportSelector;
 
-//@ComponentScan(basePackages = "com.luban",excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {AppconfigSimple.class})})
-//@Configuration
-//@Import({MyImportBeanDefinitionRegistrar.class, MyImportSelectorTest.class})
-//@ImportResource("classpath:spring.xml")
+@ComponentScan(basePackages = "com.luban",excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {AppconfigSimple.class})})
+@Configuration
+@Import({MyImportBeanDefinitionRegistrar.class, MyImportSelectorTest.class})
+@ImportResource("classpath:spring.xml")
 // proxy-target-class="true" -- 代表使用 cglib 代理，
 // 默认采用的是jdk 代理，但也只会代理是接口的，普通的类有内部判断非接口时会设置 proxyFactory.setProxyTargetClass(true);后使用 cglib代理！！
 //@EnableAspectJAutoProxy(proxyTargetClass = true)
